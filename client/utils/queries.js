8bd97fe19da53cae5274cb5fc42af827fc8b5e37
Ben Schwendiman <bschwendiman90@gmail.com>
2024-08-01 19:32:45 -0600

    client/utils/ queries and mutations

---
+++ client/utils/queries.js
@@ -0,0 +1,39 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_BUILD = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      quantity
+      image
+      category {
+        _id
+      }
+    }
+  }
+`;
+
+
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
