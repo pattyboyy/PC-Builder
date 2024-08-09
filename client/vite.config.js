ec08b7170b3067972bc82a3fa144926101215aba
pattyboyy <134738449+pattyboyy@users.noreply.github.com>
2024-08-08 19:18:28 -0600

    added cors

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:3000',
         secure: false,
         changeOrigin: true
       }
